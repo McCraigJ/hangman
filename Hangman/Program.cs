3977fd4e63eb9e73dc9b803a11ae96db703fee59
Craig Jenkins <mccraigj@gmail.com>
2019-07-19 11:55:07 +0100

    First commit

---
+++ Hangman/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hangman
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World 2!");
+
+      Console.Read();
+    }
+  }
+}
